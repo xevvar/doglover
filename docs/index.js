8da34a5180ad618517d6493cfc6eac3ea2b34e3c
Anthony <gachinigaithuma@gmail.com>
2023-11-05 19:24:30 +0300

    data

--- docs/index.js
+++ docs/index.js
@@ -1,66 +1,130 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const baseURL = "https://api.thedogapi.com/";
-    const api_key = "live_nYZH9vgM9WHddxXXIubuYxzPBhGJTkiDtZrYsCaZpwtRY3OybQRQ7nEy2t7itEYD";
-    const dogName = document.getElementById('dog-name');
-    const dogImage = document.getElementById('dog-image');
-    const searchDog = document.getElementById('search-dog');
-    const submitBtn = document.getElementById('submitBtn');
-    
+  const baseURL = "https://api.thedogapi.com/";
+  const api_key = "live_nYZH9vgM9WHddxXXIubuYxzPBhGJTkiDtZrYsCaZpwtRY3OybQRQ7nEy2t7itEYD";
+  const dogName = document.getElementById('dog-name');
+  const dogImage = document.getElementById('dog-image');
+  const searchDog = document.getElementById('search-dog');
+  const submitBtn = document.getElementById('submitBtn');
+  const reviewList = document.getElementById('review-list');
+  const reviewForm = document.getElementById('review-form');
+  const btn = document.getElementById('btn');
+  const breedGroup = document.getElementById('breed-group');
+  const breedFor = document.getElementById('breed-for');
+  const temperament = document.getElementById('temperament');
+  const lifespan = document.getElementById('lifespan');
 
 
-    var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("x-api-key", api_key);
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-var link = baseURL + "v1/images/search?format=json&limit=50" 
 
-fetch(link, requestOptions)
-  .then(response => response.text())
-//   .then(result => console.log(result))
-  .then((result) => {
-    const resultParsed = JSON.parse(result);
-    // console.log(result);
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("x-api-key", api_key);
 
-    searchDog.innerHTML = '';
-    resultParsed.forEach((main) => {
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  var link = baseURL + "v1/images/search?format=json&limit=100"
+  const populatedNames = [];
+  const populatedData = [];
+  fetch(link, requestOptions)
+    .then(response => response.text())
+    //   .then(result => console.log(result))
+    .then((result) => {
+      const resultParsed = JSON.parse(result);
+      // console.log(result);
+
+      searchDog.innerHTML = '';
+      resultParsed.forEach((main) => {
         const breeds = main.breeds;
         breeds.forEach((breed) => {
-            console.log(breed.name);
-            
+          console.log(breed.name);
+          if (populatedNames.includes(breed.name)) {
+
+          } else {
+
+            var dog = {};
+            dog.name = breed.name;
+            dog.breedFor = breed.bred_for;
+            dog.temperament = breed.temperament;
+            dog.life_span = breed.life_span;
+            dog.breedGroup = breed.breed_group;
+
+            populatedData.push(dog);
+
             const opt = document.createElement('option');
-        opt.textContent = breed.name;
-        searchDog.appendChild(opt);
+            opt.textContent = breed.name;
+            opt.value = populatedData.length - 1;
+            searchDog.appendChild(opt);
+
+            populatedNames.push(breed.name);
+          }
 
         })
-
-        
-
-        
-
-        
-    })
-    console.log(submitBtn.innerHTML);
-    submitBtn.addEventListener('click', function(){
+      })
+      console.log(submitBtn.innerHTML);
+      submitBtn.addEventListener('click', function () {
         dogName.textContent = searchDog.value;
-                //     beerImage.src = beer.image_url;
-    //     beerImage.alt = beer.name;
-    //     beerDescription.textContent = beer.description;
-    //     reviewList.innerHTML = '';
-    //     beer.reviews.forEach((review) => {
-    //         const reviewLi = document.createElement('li');
-    //         reviewLi.textContent = review;
+        fetch(link, requestOptions)
+          .then(response => response.text())
+          .then((result) => {
+            const resultParsed = JSON.parse(result);
+            breedGroup.innerHTML = '';
+            breedFor.innerHTML = '';
+            lifespan.innerHTML = '';
+            temperament.innerHTML = '';
+            resultParsed.forEach((main) => {
+              const breeds = main.breeds;
 
-    //         reviewList.appendChild(reviewLi);
-    //     })
+              breeds.forEach((breed) => {
+                if (breed.name == searchDog.value) {
+                  dogImage.src = breed.url;
+                  dogImage.alt = breed.name;
+                  breedGroup.textContent = breed.breed_group;
+                  breedFor.textContent = breed.bred_for;
+                  temperament.textContent = breed.temperament;
+                  lifespan.textContent = breed.life_span;
+                }
+              })
+            })
+          })
+
+        //     beerImage.src = beer.image_url;
+        //     beerImage.alt = beer.name;
+        //     beerDescription.textContent = beer.description;
+
+        //     beer.reviews.forEach((review) => {
+        //         
+
+        //         
+        //     })
+
+      });
+
+      // reviewList.innerHTML = '';
+      // const reviewLi = document.createElement('li');
+      // reviewLi.textContent = review;
+      // reviewList.appendChild(reviewLi);
 
 
-     });
 
-}).catch(error => console.log('error', error));
+    }).catch(error => console.log('error', error));
+
+  reviewForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    const review = document.getElementById('review').value;
+
+    const reviewItem = document.createElement('li');
+    reviewItem.innerHTML = `${review}`;
+
+    reviewList.appendChild(reviewItem);
+
+    reviewForm.reset();
+
+  });
 
 });
+
